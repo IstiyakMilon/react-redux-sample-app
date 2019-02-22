42831708a05596347f0c6dfdea75ec2f77b584f4
Istiyak Milon <milon.istiyak@gmail.com>
2019-02-22 14:48:59 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Scoreboard from './containers/Scoreboard'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Scoreboard />
+      </div>
+    );
+  }
+}
+
+export default App;
